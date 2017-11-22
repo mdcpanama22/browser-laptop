fd631816ce475dd7b4c017450a366d664b542610
Pete Miller <miller.pete+commit@gmail.com>
2017-11-22 08:57:13 -0800

    Improve UI of new tab page(s) with fonts and Private Search styles

--- js/about/newprivatetab.js
+++ js/about/newprivatetab.js
@@ -33,36 +33,30 @@ class NewPrivateTab extends React.Component {
         <Stats newTabData={this.props.newTabData} />
         <Clock />
       </div>
-      <div className={css(styles.wrapper)}>
+      <div className={css(styles.section_privateTab, styles.wrapper)}>
         <div className={css(styles.iconGutter, styles.lionImage)} />
         <div className={css(styles.textWrapper)}>
           <h1 className={css(styles.title)} data-l10n-id='privateTabTitle' />
           <p className={css(styles.text)} data-l10n-id='privateTabText1' />
-          <p className={css(styles.text, styles.italic)} data-l10n-id='privateTabText3' />
+          <p className={css(styles.text, styles.text_thirdPartyNote)} data-l10n-id='privateTabText3' />
         </div>
       </div>
       {
         this.props.newTabData.hasIn(useAlternativePrivateSearchEngineDataKeys) &&
-        <div className={css(styles.wrapper)}>
+        <div className={css(styles.section_privateSearch, styles.wrapper)}>
           <div className={css(styles.iconGutter)}>
             <img className={css(styles.ddgImage)} src={ddgIcon} alt='DuckDuckGo logo' />
           </div>
           <div className={css(styles.textWrapper)}>
             <SettingCheckbox
+              large
               switchClassName={css(styles.newPrivateTab__switch)}
-              rightLabelClassName={css(styles.newPrivateTab__switch__label)}
-              dataL10nId='useDuckDuckGoForPrivateSearch'
+              leftLabelClassName={css(styles.sectionTitle)}
+              dataL10nIdLeft='privateTabSearchSectionTitle'
               checked={Boolean(this.props.newTabData.getIn(useAlternativePrivateSearchEngineDataKeys))}
               onChange={this.onChangePrivateSearch.bind(this)}
             />
-            {/* <h2 className={css(styles.sectionTitle)} data-l10n-id='privateTabSearchSectionTitle' /> */}
-            <p className={css(styles.text)} data-l10n-id='privateTabSearchText1' />
-            <a
-              className={css(styles.newPrivateTab__ddgLink)}
-              href='https://duckduckgo.com/about'
-            >
-              DuckDuckGo and your privacy
-            </a>
+            <p className={css(styles.text, styles.text_privateSearch)} data-l10n-id='privateTabSearchText1' />
           </div>
       </div>
       }
@@ -98,12 +92,19 @@ const styles = StyleSheet.create({
     padding: '40px 60px' // same as newtab
   },
 
+  section_privateTab: {
+    margin: '20px 0 0 0'
+  },
+
+  section_privateSearch: {
+    marginTop: '40px'
+  },
+
   wrapper: {
+    fontFamily: 'Muli',
     display: 'flex',
     alignSelf: 'center',
     maxWidth: '780px',
-    margin: `${globalStyles.spacing.privateTabPaddingHorizontal} 0`,
-    marginBottom: 0,
 
     [atBreakpoint]: {
       flexDirection: 'column'
@@ -111,7 +112,8 @@ const styles = StyleSheet.create({
   },
 
   textWrapper: {
-    margin: `0 ${globalStyles.spacing.privateTabPadding}`,
+    fontFamily: 'inherit',
+    marginLeft: '25px',
     marginBottom: 0,
     [atBreakpoint]: {
       padding: '14px 0',
@@ -149,39 +151,50 @@ const styles = StyleSheet.create({
   },
 
   title: {
+    fontFamily: 'inherit',
     color: globalStyles.color.white100,
     fontSize: '30px',
     marginTop: '14px',
-    marginBottom: globalStyles.spacing.privateTabPaddingHorizontal
+    marginBottom: '24px'
   },
 
   sectionTitle: {
+    fontFamily: 'inherit',
     color: globalStyles.color.white100,
-    fontSize: '24px',
-    marginBottom: globalStyles.spacing.privateTabPaddingHorizontal
+    fontSize: '24px'
   },
 
   text: {
+    lineHeight: '1.4',
+    fontSize: '18px',
+    color: globalStyles.color.alphaWhite,
+    maxWidth: '544px',
+    fontFamily: 'inherit',
     ':not(:last-of-type)': {
-      paddingBottom: globalStyles.spacing.privateTabPaddingHorizontal
-    },
-    lineHeight: '1.5',
-    fontSize: '17px',
-    color: globalStyles.color.alphaWhite
+      paddingBottom: '20px'
+    }
   },
 
-  italic: {
-    fontStyle: 'italic'
+  text_privateSearch: {
+    fontSize: '17px',
+    lineHeight: '1.5'
+  },
+
+  text_thirdPartyNote: {
+    fontStyle: 'italic',
+    fontSize: '15px'
   },
 
   newPrivateTab__switch: {
     marginTop: '7px',
-    marginBottom: '14px'
+    marginBottom: '20px',
+    padding: 0
   },
 
   newPrivateTab__switch__label: {
+    fontFamily: 'Muli',
     color: globalStyles.color.alphaWhite,
-    fontWeight: 'bold'
+    fontSize: '18px'
   },
 
   newPrivateTab__ddgLink: {
